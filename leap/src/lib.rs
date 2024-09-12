8524b72b6866216a467c130d36a8c1aa0144599b
Ian Evans <icevans@icloud.com>
2024-09-11 17:31:33 -0700

    refactor leap year solution
    Defines a DivisibleBy trait and implements it for u64, then uses
    that in our leap year solution. Good chance to implement a trait
    on an external type, and makes the solution slightly more semantic.

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,7 +1,17 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if year % 100 == 0 {
-        return year % 400 == 0;
+    if year.divisible_by(100) {
+        return year.divisible_by(400);
     }
 
-    year % 4 == 0
+    year.divisible_by(4)
+}
+
+trait DivisibleBy {
+    fn divisible_by(self, n: Self) -> bool;
+}
+
+impl DivisibleBy for u64 {
+    fn divisible_by(self, n: Self) -> bool {
+        self % n == 0
+    }
 }
