6d5d34ad1f0242aeb247a58d52218c074aa3b021
Ian Evans <icevans@icloud.com>
2024-09-11 19:52:16 -0700

    Provides blanket implementation of DivisibleBy
    Instead of implementing only for u64, we provide a blanket implementation
    of DivisibleBy for any type that we can ask for the remainder of given
    another instance of that type, and get back a value of that type. Which
    includes, logically, anything that is a number in a fairly broad sense.

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Rem;
+
 pub fn is_leap_year(year: u64) -> bool {
     if year.divisible_by(100) {
         return year.divisible_by(400);
@@ -10,8 +12,11 @@ trait DivisibleBy {
     fn divisible_by(self, n: Self) -> bool;
 }
 
-impl DivisibleBy for u64 {
+impl<T> DivisibleBy for T
+where
+    T: Rem<Output = T> + PartialEq + From<u8>,
+{
     fn divisible_by(self, n: Self) -> bool {
-        self % n == 0
+        self % n == 0u8.into()
     }
 }
