ce55f1b97596ae4576ad2df4087682d107ef1797
Ian Evans <icevans@icloud.com>
2024-09-28 07:53:32 -0700

    first pass at diffie hellman

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,11 +1,29 @@
+use rand::prelude::*;
+
 pub fn private_key(p: u64) -> u64 {
-    todo!("Pick a private key greater than 1 and less than {p}")
+    let mut rng = thread_rng();
+    let x: u64 = rng.gen_range(2..p);
+
+    x
 }
 
-pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    todo!("Calculate public key using prime numbers {p} and {g}, and private key {a}")
+pub fn public_key(p: u64, g: u64, private_key: u64) -> u64 {
+    // public_key = g^private_key mod p
+    let mut key = 1u64;
+
+    for _ in 0..private_key {
+        key = (key.wrapping_mul(g)) % p;
+    }
+
+    key
 }
 
-pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    todo!("Calculate secret key using prime number {p}, public key {b_pub}, and private key {a}")
+pub fn secret(p: u64, other_public_key: u64, private_key: u64) -> u64 {
+    let mut secret = 1u64;
+
+    for _ in 0..private_key {
+        secret = (secret.wrapping_mul(other_public_key)) % p;
+    }
+
+    secret
 }
